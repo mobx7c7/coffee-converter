23caa6f7942772c4a49edd03e2d72ce3ee0389f0
Marcelo de Oliveira Bittencourt <mcrmaxx@gmail.com>
2021-11-16 13:08:29 -0300

    Create functions

--- src/services/job.ts
+++ src/services/job.ts
@@ -1,18 +1,84 @@
 import mongoose from "mongoose";
 import Batch from '../models/batch';
-import Job from '../models/job'
+import Job from '../models/job';
 import { Status } from '../common/consts';
 
 class JobService {
     /**
+     * Selects a job
+     * 
+     * @param {string} id The resource id
+     * @returns {Promise<any>}
+     */
+    async select(id: string): Promise<any> {
+        return await Job.findById(id);
+    }
+    /**
+     * Selects all jobs
+     * 
+     * @param {Object.<any>} filters Query parameters
+     * @param {Object.<number>} offset Result position
+     * @param {Object.<number>} length Amount of resources per result
+     * @returns {Promise<any>}
+     */
+    async selectAll({
+        filters = null,
+        offset = 0,
+        length = 10
+    }: {
+        filters?: any,
+        offset?: number,
+        length?: number
+    } = {}): Promise<any> {
+        let query
+
+        if (filters) {
+            let fields = ['userId']
+            query = fields.reduce((q, f) => {
+                if (f in filters) {
+                    q[f] = { $eq: filters[f] }
+                }
+                return q
+            }, {})
+        }
+
+        return await Batch
+            .find(query)
+            .populate({
+                path: 'jobs',
+                select: [
+                    '_id',
+                    'status',
+                    'title',
+                    'oFile',
+                    'createdAt',
+                    'startedAt',
+                    'finishedAt',
+                ]
+            })
+            .select(['jobs'])
+            .skip(offset)
+            .limit(length)
+            .sort({ createdAt: 'asc' })
+            .lean(true)
+            .select([
+                '_id',
+                'status',
+                'params',
+                'createdAt',
+                'startedAt',
+                'finishedAt',
+                'jobs',
+            ]);
+    }
     /**
      * Creates a batch
-    * 
+     * 
      * @param {Object.<any>} files List of files received from user
      * @param {Object.<number>} params Batch parameters for all jobs
      * @param {Object.<number>} userId Owner id
-    * @returns {Promise<any>}
-    */
+     * @returns {Promise<any>}
+     */
     async createBatch({
         files = {},
         params = '',
@@ -43,38 +109,18 @@ class JobService {
             jobs: jobs.map(j => j._id),
         });
 
-        await batch.populate({
+        return await batch.populate({
             path: 'jobs',
             select: [
                 '_id',
                 'status',
                 'title',
                 'oFile',
+                'createdAt',
                 'startedAt',
                 'finishedAt',
-                'createdAt',
             ]
-        })
-
-        return {
-            id: batch._id,
-            status: batch.status,
-            params: batch.params,
-            userId: batch.userId,
-            jobs: batch.jobs.map(j => {
-                return {
-                    id: j._id,
-                    status: j.status,
-                    title: j.title,
-                    downloadUrl: j.oFile,
-                    createdAt: j.createdAt || undefined,
-                    startedAt: j.startedAt,
-                    finishedAt: j.finishedAt || undefined,
-                }
-            }),
-            createdAt: batch.createdAt,
-            updatedAt: batch.updatedAt
-        }
+        });
     }
 }
 
