b63c76a264ee0b250331aa64ceff7a5b4e50d451
Marcelo de Oliveira Bittencourt <mcrmaxx@gmail.com>
2021-11-15 22:48:01 -0300

    Create consts file

---
+++ src/common/consts.ts
@@ -0,0 +1,8 @@
+export const Status = Object.freeze({
+    DEFAULT: 'default',
+    WAITING: 'waiting',
+    PROCESSING: 'processing',
+    SUCCEDED: 'succeded',
+    FAILED: 'failed',
+    ABORTED: 'aborted'
+})
