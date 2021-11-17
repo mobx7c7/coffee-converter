2161ec71686627d9888a1823e2739f9431bb7d4b
Marcelo de Oliveira Bittencourt <mcrmaxx@gmail.com>
2021-11-17 00:46:57 -0300

    Improve transcoder service

--- src/services/transcoder.ts
+++ src/services/transcoder.ts
@@ -1,40 +1,68 @@
-import Bull, { Queue, Job, JobStatusClean } from 'bull';
-import chalk from 'chalk';
-import log from '../log';
+import Bull, { Job, JobStatusClean, Queue } from 'bull';
 import fs from 'fs';
+import log from '../log';
+import {
+    Transcoder,
+    TranscoderEvents,
+    TranscoderFactory
+} from '../common/media/transcoder';
 import JobService from './job';
 import { Status } from '../common/consts';
 
+function logEventBase(
+    logger: any,
+    message: any,
+): void {
+    logger('transcoder_service', message);
+}
+
+function logInfo(
+    message: string
+): void {
+    logEventBase(log.info, message);
+}
+
+function logWarn(
+    message: string
+): void {
+    logEventBase(log.warn, message);
+}
+
+function logFail(
+    message: string
+): void {
+    logEventBase(log.error, message);
+}
+
+interface TranscoderData {
+    transcoder?: Transcoder;
+    oStream?: fs.WriteStream;
+}
+
 export default class TranscoderService {
+    private processes: TranscoderData[];
     private mainQueue: Queue<any>;
 
     constructor() {
+        this.processes = [];
         this.mainQueue = new Bull('transcoder');
         this.initQueue(this.mainQueue);
     }
 
-    private async clearQueue(queue: Queue<any>): Promise<void> {
+    protected async clearQueue(
+        queue: Queue<any>
+    ): Promise<void> {
         let jobStatusCleanList = ['completed', 'wait', 'active', 'delayed', 'failed'];
         jobStatusCleanList.forEach(async (v: JobStatusClean) => await queue.clean(0, v))
         await queue.removeJobs('*');
     }
 
-    private async initQueue(queue: Queue<any>): Promise<Queue<any>> {
-        function logEventBase(message, logger, color) {
-            logger('transcoder', message)
-        }
-        function logInfo(message) {
-            logEventBase(message, log.info, chalk.green)
-        }
-        function logWarn(message) {
-            logEventBase(message, log.warn, chalk.yellow)
-        }
-        function logFail(message) {
-            logEventBase(message, log.error, chalk.red)
-        }
+    protected async initQueue(
+        queue: Queue<any>
+    ): Promise<Queue<any>> {
         return queue
             .on('error', function (error) {
-                logFail(`An error ocurred: ${error}`)
+                logFail(`An error ocurred: ${error}`);
             })
             .on('waiting', function (jobId) {
                 logWarn(`Job ${jobId} is waiting to be processed`)
@@ -49,7 +77,7 @@ export default class TranscoderService {
                 logInfo(`Job ${job.id} is completed. Result: ${JSON.stringify(result)}`)
             })
             .on('progress', function (job, progress) {
-                logInfo(`Job ${job.id} is ${progress * 100}% completed`)
+                logInfo(`Job ${job.id} is ${progress}% completed`)
             })
             .on('failed', function (job, err) {
                 logFail(`Job ${job.id} failed: ${err.message}`)
@@ -68,11 +96,63 @@ export default class TranscoderService {
             })
             .on('cleaned', function (jobs, type) {
                 if (jobs.length === 0) {
-                    logWarn(`No ${type} jobs to remove`)
+                    this.logWarn(`No ${type} jobs to remove`)
                 } else {
-                    logWarn(`The following ${type} jobs were removed: ${JSON.stringify(jobs)}`)
+                    this.logWarn(`The following ${type} jobs were removed: ${JSON.stringify(jobs)}`)
                 }
             })
+    }
+
+    protected async notifyJobStart(
+        id: string,
+        status: string
+    ): Promise<any> {
+        return await JobService.updateOne({
+            id: id,
+            input: {
+                status: status,
+                startedAt: new Date().toISOString()
+            }
+        });
+    }
+
+    protected async notifyJobFinish(
+        id: string,
+        status: string
+    ): Promise<any> {
+        await this.destroyProcess(id, status == Status.FAILED);
+        return await JobService.updateOne({
+            id: id,
+            input: {
+                status: status,
+                finishedAt: new Date().toISOString()
+            }
+        });
+    }
+
+    protected async destroyProcess(
+        jobId: string,
+        errored = false
+    ): Promise<void> {
+        let process = this.processes[jobId];
+        process.oStream.close();
+        if (errored) {
+            await fs.promises.unlink(process.oStream.path);
+        }
+        delete this.processes[jobId];
+    }
+
+    protected createProcess(
+        jobId: string,
+        iFile: string,
+        oFile: string
+    ): TranscoderData {
+        let process = {
+            oStream: fs.createWriteStream(oFile),
+            transcoder: TranscoderFactory.audioFormat(iFile),
+        }
+        this.processes[jobId] = process;
+        return process;
     }
 
     async getJobs(): Promise<Job<any>[]> {
@@ -88,28 +168,35 @@ export default class TranscoderService {
     }
 
     async process(): Promise<void> {
-        this.mainQueue.process(async function (job, done) {
-            let { jobId, iFile, oFile, params } = job.data;
-
-            await JobService.updateStatus(jobId, Status.PROCESSING);
-
-            function fakeProgress(intervalMs = 1000) {
-                let progress = 0;
-                let timer = setInterval(async () => {
-                    if (progress < 100) {
-                        job.progress(progress / 100)
-                        progress += 10;
-                    } else {
-                        await fs.promises.copyFile(iFile, oFile);
-                        await JobService.updateStatus(jobId, Status.SUCCEDED);
+        this.mainQueue.process((job, done) => {
+            try {
+                let { jobId, iFile, oFile } = job.data;
+                let process = this.createProcess(jobId, iFile, oFile);
+                process.transcoder
+                    .on(TranscoderEvents.PROGRESS, (e) => {
+                        job.progress(e.progress.percent.toFixed());
+                    })
+                    .on(TranscoderEvents.STARTED, () => {
+                        this.notifyJobStart(jobId, Status.PROCESSING);
+                        logInfo(`Process started for job ${job.id}`);
+                    })
+                    .on(TranscoderEvents.FAILED, () => {
+                        this.notifyJobFinish(jobId, Status.FAILED);
+                        logInfo(`Process failed for job ${job.id}`);
                         done();
-                        clearInterval(timer);
-                    }
-                }, intervalMs);
+                    })
+                    .on(TranscoderEvents.FINISHED, () => {
+                        this.notifyJobFinish(jobId, Status.SUCCEDED);
+                        logInfo(`Process finished for job ${job.id}`);
+                        done();
+                    })
+                    .on(TranscoderEvents.DATA, (e) => {
+                        process.oStream.write(e.chunk);
+                    })
+                    .start();
+            } catch (error) {
+                throw error;
             }
-
-            // TODO: Replace by ffmpeg
-            fakeProgress(500)
         })
     }
 }
